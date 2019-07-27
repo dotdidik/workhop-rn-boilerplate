7994ffd044316cefe479ba6bc762c144586bbe3b
Didik Irawan <didikirawan@Didiks-MacBook-Pro.local>
2019-07-27 16:19:00 +0700

    add server

--- src/components/common/mainCard.js
+++ src/components/common/mainCard.js
@@ -1,43 +1,28 @@
 import React, { Component } from 'react';
 import { Image, View } from 'react-native';
 import { Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
-
-export default class MainCard extends Component {
+import axios from 'axios'
+import {withNavigation} from 'react-navigation'
+class MainCard extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            obj: {
-                name: "Didik",
-                age: 19,
-                photo: 'https://cdn0-production-images-kly.akamaized.net/tAr72vTJCpF4IF9O5L493CD79kE=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2754932/original/005940800_1552970791-fotoHL_kucing.jpg'
-            },
-            arr: [
-                {
-                    name: "Didik 1",
-                    age: 19,
-                    photo: 'https://cdn0-production-images-kly.akamaized.net/tAr72vTJCpF4IF9O5L493CD79kE=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2754932/original/005940800_1552970791-fotoHL_kucing.jpg'
-                },
-                {
-                    name: "Didik 2",
-                    age: 19,
-                    photo: 'https://cdn0-production-images-kly.akamaized.net/tAr72vTJCpF4IF9O5L493CD79kE=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2754932/original/005940800_1552970791-fotoHL_kucing.jpg'
-                },
-                {
-                    name: "Didik 3",
-                    age: 19,
-                    photo: 'https://cdn0-production-images-kly.akamaized.net/tAr72vTJCpF4IF9O5L493CD79kE=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2754932/original/005940800_1552970791-fotoHL_kucing.jpg'
-                },
-                {
-                    name: "Didik 4",
-                    age: 19,
-                    photo: 'https://cdn0-production-images-kly.akamaized.net/tAr72vTJCpF4IF9O5L493CD79kE=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2754932/original/005940800_1552970791-fotoHL_kucing.jpg'
-                }
-            ]
+            arr: []
         }
     }
 
+    componentDidMount(){
+        axios.get('https://article.baggrek.com/mobile/api/v1/article')
+            .then(res => {
+                this.setState({
+                    arr : res.data.data.result
+                })
+            })
+    }
+
     render() {
+        console.log('data', this.state.arr)
         return (
             <View>
                 {
@@ -47,21 +32,26 @@ export default class MainCard extends Component {
                                 <Left>
                                     <Thumbnail source={{ uri: 'https://cdn0-production-images-kly.akamaized.net/tAr72vTJCpF4IF9O5L493CD79kE=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2754932/original/005940800_1552970791-fotoHL_kucing.jpg' }} />
                                     <Body>
-                                        <Text>{person.name}</Text>
-                                        <Text note>{person.age}</Text>
+                                        <Text>{person.article.username}</Text>
                                     </Body>
                                 </Left>
                             </CardItem>
+                            <Text>{person.article.status}</Text>
                             <CardItem cardBody>
-                                <Image source={{ uri: person.photo }} style={{ height: 200, width: null, flex: 1 }} />
+                                <Image source={{ uri: person.article.photo.url }} style={{ height: 200, width: null, flex: 1 }} />
                             </CardItem>
                             <CardItem>
                                 <Left>
                                 </Left>
                                 <Body>
-                                    <Button transparent>
+                                    <Button transparent 
+                                        onPress={() => {
+                                            /* 1. Navigate to the Details route with params */
+                                            this.props.navigation.navigate('Details', {person});
+                                          }}
+                                    >
                                         <Icon active name="chatbubbles" />
-                                        <Text>4 comm</Text>
+                                        <Text>{person.comment.length} comments</Text>
                                     </Button>
                                 </Body>
                                 <Right />
@@ -73,3 +63,5 @@ export default class MainCard extends Component {
         );
     }
 }
+
+export default withNavigation(MainCard)
--- src/components/view/detail/index.js
+++ src/components/view/detail/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Text, View, Button } from 'react-native';
 
 class DetailsScreen extends React.Component {
+
     render() {
+      console.log('this is detail', this.props)
       return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Text>Details!</Text>
